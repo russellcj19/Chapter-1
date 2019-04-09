fdd3d9fa646a081a9eb2bde4092e082062356812
Russell <CR133362@davenportschools.org>
2019-04-09 12:35:19 -0500

    Initial commit

---
+++ src/TableAndChairs.java
@@ -0,0 +1,22 @@
+public class TableAndChairs {
+    //Author: Connor Russell
+    //Date: 04/09/2019
+    //Project: Table and Chairs
+    public static void main(String[] args){
+
+        System.out.println("Building Some Tables And Chairs");
+        System.out.println("\n\n\n\n");
+
+        System.out.println("C                                            C");
+        System.out.println("H                                            H");
+        System.out.println("A                                            A");
+        System.out.println("I           ---------------------            I");
+        System.out.println("R             X               X              R");
+        System.out.println("CHAIRCHAIR    X               X     CHAIRCHAIR");
+        System.out.println("C        C    X               X     C        C");
+        System.out.println("H        H    X               X     H        H");
+        System.out.println("A        A    X               X     A        A");
+        System.out.println("I        I    X               X     I        I");
+        System.out.println("R        R    X               X     R        R");
+    }
+}
