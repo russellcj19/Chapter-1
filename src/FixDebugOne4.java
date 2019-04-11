5d09173dfba36e9782794778193e4d0e1ef692b3
Russell <CR133362@davenportschools.org>
2019-04-11 12:44:05 -0500

    update all

---
+++ src/FixDebugOne4.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+
+public class FixDebugOne4 {
+    public static void main(String[] args)
+    {
+        JOptionPane.showMessageDialog(null, "First GUI program");
+    }
+}
--- src/RandomGuess.java
+++ src/RandomGuess.java
@@ -1,2 +1,8 @@
+
+import javax.swing.*;
+
 public class RandomGuess {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null,"The number is "+(1 + (int)(Math.random() * 10)));
+    }
 }
